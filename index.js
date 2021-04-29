d6ea14d0242060365a4a30a291968a9bc7df4e64
nonuabi <nonuabi@gmail.com>
2021-04-29 17:09:26 +0530

    add home controller, router, intall ejs and make home.ejs file, change view engine to views

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const app = express();
 const port = 8000;
+const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use("/", require("./routes"));
 
 app.listen(port, function (err) {
   if (err) {
