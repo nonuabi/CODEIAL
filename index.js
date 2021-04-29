76a05d05715dc3f97316e8661758f579eaa20842
nonuabi <nonuabi@gmail.com>
2021-04-29 17:55:27 +0530

    extract style and scritps from sub pages into the layouts

--- index.js
+++ index.js
@@ -3,12 +3,25 @@ const port = 8000;
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 
+
+//use express layouts functionality
 app.use(expressLayouts);
 
+//extract style and scripts 
+//from sub pages into the  layouts
+app.set('layout extractStyles', true);
+app.set("layout extractScripts", true);
+
+
+
+//set up the view engine
 app.set("view engine", "ejs");
 app.set("views", "./views");
+
+//use static files
 app.use(express.static("./assets"));
 
+//use express router
 app.use("/", require("./routes"));
 
 app.listen(port, function (err) {
