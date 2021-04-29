b31ac5c4d22f53ab1946774208f855941df46aa1
nonuabi <nonuabi@gmail.com>
2021-04-29 21:42:35 +0530

    created forms for sign up and sign in files

--- controller/user_profile_controller.js
+++ controller/user_profile_controller.js
@@ -1,8 +1,8 @@
-module.exports.profile = function(req, res){
-    return res.render('user_profile', {
-        title : "User Profile",
-    })
-}
+module.exports.profile = function (req, res) {
+  return res.render("user_profile", {
+    title: "User Profile",
+  });
+};
 
 module.exports.signUp = function (req, res) {
   return res.render("user_signUp", {
@@ -14,4 +14,14 @@ module.exports.signIn = function (req, res) {
   return res.render("user_signIn", {
     title: "Codeial | SignIn",
   });
+};
+
+//get the sign up data
+module.exports.create = function (req, res) {
+  //To-do later
+};
+
+//sign in and create a session for the user
+module.exports.createSession = function (req, res) {
+  //todo-lists
 };
