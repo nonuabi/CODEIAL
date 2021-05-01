a7ee708db182e8e85dd98d5b9f3f19ca02e34a4c
nonuabi <nonuabi@gmail.com>
2021-05-01 13:11:27 +0530

    display posts and related user

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,7 +1,36 @@
+const Post = require("../models/post");
+
 module.exports.home = function (req, res) {
   // console.log(req.cookies);
   // res.cookie("user_id", 25);
-  return res.render("home", {
-    title: "home",
-  });
+  // return res.render("home", {
+  //   title: "home",
+  // });
+
+  // Post.find({}, function (err, post) {
+  //   if (err) {
+  //     console.log(`error in fetch the user post data
+  //     from the db :: ${err}`);
+  //     return;
+  //   }
+  //   return res.render("home", {
+  //     title: "Home",
+  //     arr: post,
+  //   });
+  // });
+
+  //populate the user of each post
+  Post.find({})
+    .populate("user")
+    .exec(function (err, post) {
+      if (err) {
+        console.log(`error in fetch the user post data 
+      from the db :: ${err}`);
+        return;
+      }
+      return res.render("home", {
+        title: "Home",
+        arr: post,
+      });
+    });
 };
