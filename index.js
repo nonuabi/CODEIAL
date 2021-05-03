955c4212dc7da2c4ffbef48a301b141aa338569b
nonuabi <nonuabi@gmail.com>
2021-05-03 16:34:38 +0530

    installed connect-flash, created flash messages

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const passport = require("passport");
 const passportLocal = require("./config/passport-local-strategy");
 const MongoStore = require("connect-mongo")(session);
 const sassMiddleware = require("node-sass-middleware");
+const flash = require("connect-flash");
+const customMware = require('./config/middleware');
+
 
 app.use(
   sassMiddleware({
@@ -69,6 +72,9 @@ app.use(passport.session());
 
 app.use(passport.setAuthenticatedUser);
 
+//it use the session cookies
+app.use(flash());
+app.use(customMware.setFlash);
 //use express router
 app.use("/", require("./routes"));
 
