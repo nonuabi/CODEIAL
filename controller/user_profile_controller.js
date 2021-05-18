373b9b314600f8a234f25aab61b3e40beb2ca0a0
nonuabi <nonuabi@gmail.com>
2021-05-18 18:27:30 +0530

    saving file from params

--- controller/user_profile_controller.js
+++ controller/user_profile_controller.js
@@ -9,20 +9,46 @@ module.exports.profile = function (req, res) {
   });
 };
 
-module.exports.update = function (req, res) {
+module.exports.update = async function (req, res) {
+  // if (req.user.id == req.params.id) {
+  //   User.findByIdAndUpdate(
+  //     req.params.id,
+  //     {
+  //       name: req.body.name,
+  //       email: req.body.email,
+  //     },
+  //     function (err, user) {
+  //       return res.redirect("back");
+  //     }
+  //   );
+  // } else {
+  //   res.status(401).send("Unauthorized");
+  // }
   if (req.user.id == req.params.id) {
-    User.findByIdAndUpdate(
-      req.params.id,
-      {
-        name: req.body.name,
-        email: req.body.email,
-      },
-      function (err, user) {
+    try {
+      let user = await User.findById(req.params.id);
+      User.uploadedAvatar(req, res, function (err) {
+        if (err) {
+          console.log(`Multer Error ::  ${err}`);
+        } else {
+          user.name = req.body.name;
+          user.email = req.body.email.id;
+
+          if (req.file) {
+            //this is saving the path of the uploaded file into the avatar field in the user
+            user.avatar = User.avatarPath + "/" + req.file.filename;
+          }
+        }
+        user.save();
         return res.redirect("back");
-      }
-    );
+      });
+    } catch (err) {
+      req.flash("error", err);
+      return res.redirect("back");
+    }
   } else {
-    res.status(401).send("Unauthorized");
+    req.flash("error", "Unauthorized");
+    return res.status(401).send("Unauthorized");
   }
 };
 
--- models/user.js
+++ models/user.js
@@ -31,15 +31,25 @@ const userSchema = new mongoose.Schema(
 // The disk storage engine gives you full
 // control on storing files to disk.
 
-let storage = multer.deskStorage({
+let storage = multer.diskStorage({
   destination: function (req, file, cb) {
     // cb -> call back function
-    cb(null, path.join(__dirname, "..", AVATAR_PATH));
+    cb(null, path.join(__dirname, "../assets", AVATAR_PATH));
   },
   filename: function (req, file, cb) {
-    cb(null, file.filename + "-" + Date.now());
+    cb(null, file.fieldname + "-" + Date.now());
+    //filename = avatar-{date.now()}
+    // date.now() -> method returns the number of milliseconds
+    //  elapsed since January 1, 1970
   },
 });
+
+//static functions
+userSchema.statics.uploadedAvatar = multer({ storage: storage }).single(
+  "avatar"
+);
+
+userSchema.statics.avatarPath = AVATAR_PATH;
 
 const user = mongoose.model("user", userSchema);
 
