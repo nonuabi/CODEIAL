30f8da3479e1b0854da1225ab262b7ed4d2cda83
nonuabi <nonuabi@gmail.com>
2021-05-19 18:26:46 +0530

    setting up the directory structure for the api's

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,9 @@ router.get("/", homeController.home);
 router.use("/user", require("./user"));
 router.use("/post", require("./post"));
 router.use("/comments", require("./comments"));
+
+router.use("/api", require("./api"));
+
 // router.post("/signUp", require("./signUp"));
 // router.use("/signIn", require("./signIn"));
 
