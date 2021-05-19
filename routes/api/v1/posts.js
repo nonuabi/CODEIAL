d6988cbc1bd51d87bb1fc892ae20e6fefba0326b
nonuabi <nonuabi@gmail.com>
2021-05-19 20:27:07 +0530

    created version 1 for api

---
+++ routes/api/v1/posts.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const postsApi = require("../../../controller/api/v1/posts_api");
+
+router.get("/", postsApi.index);
+
+module.exports = router;
