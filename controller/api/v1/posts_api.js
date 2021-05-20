895f31536eff9cbc73ee102f11239bdfc6871fed
nonuabi <nonuabi@gmail.com>
2021-05-20 12:13:48 +0530

    created a v1 api to fetch data from the database

--- controller/api/v1/posts_api.js
+++ controller/api/v1/posts_api.js
@@ -1,6 +1,34 @@
-module.exports.index = function (req, res) {
+const Post = require("../../../models/post");
+const Comments = require("../../../models/comment");
+module.exports.index = async function (req, res) {
+  let posts = await Post.find({})
+    .sort("-createdAt")
+    .populate("user")
+    .populate({
+      path: "comments",
+      populate: {
+        path: "user",
+      },
+    });
+
   return res.json(200, {
     message: "list of post",
-    posts: [],
+    posts: posts,
   });
+};
+
+module.exports.destroy = async function (req, res) {
+  try {
+    let post = await Post.findById(req.params.id);
+    post.remove();
+    await Comments.deleteMany({ post: req.params.id });
+    return res.json(200, {
+      message: "Post and associated comments deleted sucessfully!",
+    });
+  } catch (err) {
+    console.log(err);
+    return res.json(500, {
+      message: "Internal server error",
+    });
+  }
 };
--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -4,5 +4,5 @@ const router = express.Router();
 const postsApi = require("../../../controller/api/v1/posts_api");
 
 router.get("/", postsApi.index);
-
+router.delete("/:id", postsApi.destroy);
 module.exports = router;
