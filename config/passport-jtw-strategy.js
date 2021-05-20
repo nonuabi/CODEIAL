c0494f9f9d300d4721a7bf6156aad7ac528584c3
nonuabi <nonuabi@gmail.com>
2021-05-20 17:33:21 +0530

    created a topen using jsonwebtoken libery

--- config/passport-jtw-strategy.js
+++ config/passport-jtw-strategy.js
@@ -5,14 +5,14 @@ const ExtractJWT = require("passport-jwt").ExtractJwt;
 const User = require("../models/user");
 
 let opts = {
-  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken,
   //this is my encrpytion and decrpytion key
   secretOrKey: "codeial",
 };
 
 passport.use(
   new JWTStrategy(opts, function (jwtPayLoad, done) {
-    User.findById(jwtPayload._id, function (err, user) {
+    User.findById(jwtPayLoad._id, function (err, user) {
       if (err) {
         console.log(`Error in finding user from JWT ::  ${err}`);
         return;
